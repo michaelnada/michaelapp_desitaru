23f1c78b0c40a8e69075c839878aca47f1ea0fad
unknown <michaelnada>
2024-02-15 16:29:41 +0900

    css 2

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1>michael`s page</h1>
-    <p className ="bold">hello!</p>
-    <p>welcome!</p>
+    <h1>I am a level one heading</h1>
+
+    <p>This is a paragraph of text. In the text is a <span>span element</span>
+and also a <a href="http://example.com">link</a>.</p>
+
+    <p>This is the second paragraph. It contains an <em>emphasized</em> element.</p>
+
+    <ul>
+      <li>Item one</li>
+      <li>Item two</li>
+      <li>Item <em>three</em></li>
+    </ul>
     </>
     
   )
